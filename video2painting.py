1f710a56992d4bc66f0c2e7a20147a0e12939e5c
Rifqi Ardia Ramadhan <33000326+rareguy@users.noreply.github.com>
2021-05-05 20:41:56 +0700

    Update video2painting.py

--- video2painting.py
+++ video2painting.py
@@ -2,6 +2,7 @@ import cv2
 import glob
 import os
 import time
+import argparse
 # Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
     """
@@ -24,13 +25,21 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     if iteration == total: 
         print()
 
+parser = argparse.ArgumentParser(description='this script will convert your normal video to painting-style video')
+parser.add_argument('source_video', metavar='source', type=str, nargs='+', help='the video filename (currently only mp4)')
+args = parser.parse_args()
+
+inputfile = args.source_video[0]
+
 try:
 	# img = cv2.imread('img.jpg')
 	# res = cv2.xphoto.oilPainting(img, 7, 1)
 
 	# Opens the Video file
-	cap= cv2.VideoCapture('tunnel.mp4')
+	cap= cv2.VideoCapture(inputfile)
+	filename = inputfile[:-4]
 	fps = cap.get(cv2.CAP_PROP_FPS)
+	print("FPS:", fps)
 	i = 0
 	print("converting video to frames...")
 	try:
@@ -42,7 +51,7 @@ try:
 		ret, frame = cap.read()
 		if ret == False:
 			break
-		cv2.imwrite('temp/temp'+str(i)+'.jpg',frame)
+		cv2.imwrite('temp/temp'+str(filename)+str(i)+'.jpg',frame)
 		i += 1
 	
 	cap.release()
@@ -57,8 +66,8 @@ try:
 	size = ()
 	printProgressBar(0, i, prefix = 'Progress:', suffix = 'Complete', length = 50)
 	for j in range(0, i):
-		img = cv2.imread('temp/temp'+str(j)+'.jpg')
-		res = cv2.xphoto.oilPainting(img, 7, 1)
+		img = cv2.imread('temp/temp'+str(filename)+str(j)+'.jpg')
+		res = cv2.xphoto.oilPainting(img, 8, 1)
 		height, width, layers = img.shape
 		size = (width,height)
 		painting_array.append(res)
@@ -67,7 +76,7 @@ try:
 	print("finished converting the frames to painting")
 	
 	print("continuing to convert the painting frames back to video...")
-	video_output = cv2.VideoWriter('combinedtunnel.avi',cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+	video_output = cv2.VideoWriter('painted_'+str(filename)+'.avi',cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
 	printProgressBar(0, len(painting_array), prefix = 'Progress:', suffix = 'Complete', length = 50)
 	for j in range(len(painting_array)):
 		video_output.write(painting_array[j])
